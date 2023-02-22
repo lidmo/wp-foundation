2720f813d5ed28ed060863f7f47d289a1860840c
Wesley Silva <wesleysilva@lidmo.com.br>
2023-02-22 01:55:06 -0300

    configurando nome e tipo de hook dinamicamente

--- src/Hooks/Hook.php
+++ src/Hooks/Hook.php
@@ -2,13 +2,24 @@
 
 namespace Lidmo\WP\Foundation\Hooks;
 
+use Illuminate\Support\Str;
 use Lidmo\WP\Foundation\Contracts\Hook as BaseHook;
 
 abstract class Hook implements BaseHook
 {
+    protected $name;
+
+    protected $type;
     protected $priority = 100;
 
     protected $acceptedArgs = 1;
+
+    public function __construct()
+    {
+        $class = explode('\\', get_class($this));
+        $this->name = Str::snake(array_pop($class));
+        $this->type = str_singular(strtolower(array_pop($class)));
+    }
 
     /**
      * @throws \Exception
